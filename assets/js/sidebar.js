73abcd17964ddc019833d51da710714913ecbc05
simonezardi <simone.zardi@storci.com>
2022-04-08 14:54:49 +0200

    Aggiunta visibilità alla voce 'Lista Clienti' del sidebar

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -9,7 +9,9 @@ let entityName = localStorage.getItem('global_entityName')
 let customer = localStorage.getItem('global_selected_customer')
 customer = customer.replace(/_/g, ' ')
 $('#id-customer-name').text(customer)
-
+if(localStorage.getItem('global_customer').includes("Storci")){
+  $('#id-nav-customers-list').removeClass('d-none')
+}
 // Recupera l'url della pagina visualizzata
 // Effettua uno split dell'url recuperato dividendo la stringa tramite lo /
 // recupera il nome della pagina
@@ -45,8 +47,8 @@ tw.service_90_sidebar(entityName)
       let idBtnAccordion         = '#id-btn-accordion-dryers'
       let idCollapsePanel        = '#id-collapse-panel-dryers'
       // link
-      let nav_dashboard_link     = '#id-nav-dashboard-dryer div a'
-      let nav_history_link       = '#id-nav-history-dryer div a'
+      let nav_dashboard_link     = '#id-nav-dashboard-dryer a'
+      let nav_history_link       = '#id-nav-history-dryer a'
 
       // Visualizza il menu delle celle
       $(idAccordion).removeClass('d-none')
